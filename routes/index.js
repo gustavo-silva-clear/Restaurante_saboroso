565ade0302724a5c2564b5d43826bbf4dfb7f198
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-20 12:32:08 -0300

    feat: displaying database data with EJS Embedded JavaScript templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+var conn = require('./../inc/db')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(` SELECT * FROM tb_menu ORDER BY title` , (err , results) => {
+
+    if(err) {
+
+      console.log(err);
+    }
+
+    res.render('index', { title: 'Restaurante Saboro' , menus: results });
+
+  })
+  
 });
 
 module.exports = router;
