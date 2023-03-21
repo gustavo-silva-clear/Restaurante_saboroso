164ce19b209ba9a42d4f0dc239d1548018605822
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-21 09:54:44 -0300

    feat: organizing HEADER with Includes

--- routes/index.js
+++ routes/index.js
@@ -3,42 +3,62 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  conn.query(` SELECT * FROM tb_menu ORDER BY title` , (err , results) => {
+  conn.query(` SELECT * FROM tb_menu ORDER BY title`, (err, results) => {
 
-    if(err) {
+    if (err) {
 
       console.log(err);
     }
 
-    res.render('index', { title: 'Restaurante Saboro' , menus: results });
+    res.render('index', { title: 'Restaurante Saboro', menus: results });
 
   })
-  
+
 });
 
-router.get('/contacts', function(req,res,next){
+router.get('/contacts', function (req, res, next) {
 
-res.render('contacts' , { title: 'Contatos - Restaurante Saboro' });
+  res.render('contacts', {
+    title: 'Contatos - Restaurante Saboro',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
 
-})
-
-router.get('/menus', function(req,res,next){
-
-  res.render('menus' , { title: 'Menus - Restaurante Saboro' });
-  
-  })
-
-router.get('/reservations', function(req,res,next){
-
-  res.render('reservations' ,  { title: 'Reservas -  Restaurante Saboro' });
+  });
 
 })
 
-router.get('/services', function(req,res,next){
+router.get('/menus', function (req, res, next) {
 
-  res.render('services' ,  { title: 'Restaurante Saboro' });
+  res.render('menus', {
+    title: 'Menus - Restaurante Saboro',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
+
+  });
+
+})
+
+router.get('/reservations', function (req, res, next) {
+
+  res.render('reservations', {
+    title: 'Reservas -  Restaurante Saboro',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
+
+  });
+
+})
+
+router.get('/services', function (req, res, next) {
+
+  res.render('services', {
+    title: 'Restaurante Saboro',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
+
+  });
 
 })
 module.exports = router;
