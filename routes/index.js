adcd5c05352b747923c1cb028d5970c5a67dcd72
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-21 11:27:29 -0300

    fix: organizing FOOTER with Includes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
-var conn = require('./../inc/db')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function (req, res, next) {
 
-  conn.query(` SELECT * FROM tb_menu ORDER BY title`, (err, results) => {
 
-    if (err) {
-
-      console.log(err);
-    }
-
-    res.render('index', { title: 'Restaurante Saboro', menus: results });
-
-  })
+  res.render('index', { title: 'Restaurante Saboro'});
 
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,9 @@
-var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-
-  conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
-
-    if (err) {
-
-      res.send(err)
-
-    }
-    else{
-
-      res.send(results);
-
-    }
-
-  });
-
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
