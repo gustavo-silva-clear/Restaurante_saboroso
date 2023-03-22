8e8a4608090e71af55a7488c3c575f58fd628fd6
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-22 12:40:44 -0300

    feat: logging in to Administration

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const user = require('../inc/users');
+var users = require('../inc/users')
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,13 +10,35 @@ router.get('/', function(req, res, next) {
 
 router.get('/login' , function(req ,res , next){
 
-res.render('admin/login');
+ users.render(req, res , null);
 
 })
 
 router.post('/login' , function(req, res, next){
 
-    
+    if(!req.body.email){
+
+      users.render(req, res, "Preencha o campo e-mail!");
+
+    }else if(req.body.password){
+
+      users.render(req, res , "preencha o campo senha!")
+
+    }else{
+
+      users.login(req.body.email, req.body.password).then(user => {
+
+      res.redirect("/admin")
+
+      }).catch(err => {
+
+      users.render(req, res , err.message || err)
+
+
+      });
+
+    }
+
 
 })
 
