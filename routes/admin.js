eb6622d7eea1b61329269223fecb9a2997cfea56
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-22 12:53:07 -0300

    feat: creating a Middleware at the router level to validate Login Session

--- routes/admin.js
+++ routes/admin.js
@@ -3,50 +3,65 @@ const user = require('../inc/users');
 var users = require('../inc/users')
 var router = express.Router();
 
+router.use(function (req, res, next) {
+
+  if (!req.session.user) {
+
+    res.redirect("/admin/login");
+  } else {
+
+    next();
+  }
+})
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('admin/index');
+router.get('/', function (req, res, next) {
+
+    res.send('admin/index');
+  
 });
 
-router.get('/login' , function(req ,res , next){
+router.get('/login', function (req, res, next) {
 
- users.render(req, res , null);
+  users.render(req, res, null);
 
 })
 
-router.post('/login' , function(req, res, next){
+router.post('/login', function (req, res, next) {
 
-    if(!req.body.email){
+  if (!req.body.email) {
 
-      users.render(req, res, "Preencha o campo e-mail!");
+    users.render(req, res, "Preencha o campo e-mail!");
 
-    }else if(req.body.password){
+  } else if (req.body.password) {
 
-      users.render(req, res , "preencha o campo senha!")
+    users.render(req, res, "preencha o campo senha!")
 
-    }else{
+  } else {
 
-      users.login(req.body.email, req.body.password).then(user => {
+    users.login(req.body.email, req.body.password).then(user => {
+
+      req.session.user = user;
 
       res.redirect("/admin")
 
-      }).catch(err => {
+    }).catch(err => {
 
-      users.render(req, res , err.message || err)
+      users.render(req, res, err.message || err)
 
 
-      });
+    });
 
-    }
+  }
 
 
 })
 
-router.get('/contacts', function(req, res, next) {
+router.get('/contacts', function (req, res, next) {
 
-    res.send('admin/contacts');
-    
-  });
+  res.send('admin/contacts');
+
+});
 
 module.exports = router;
 
