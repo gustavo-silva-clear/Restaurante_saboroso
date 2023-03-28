a0e191e263ffdabd212d1726c41aa9eba86e36a9
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-03-28 12:30:32 -0300

    feat: user administration creating screens

--- public/js/grid.js
+++ public/js/grid.js
@@ -23,7 +23,13 @@ class grid {
             formCreate: '#modal-create form',
             formUpdate: '#modal-update form',
             btnUpdate: '.btn-update',
-            btnDelete: '.btn-delete'
+            btnDelete: '.btn-delete',
+            onUpdateload: (form, name, data) => {
+
+              let input =  form.querySelector('[name=' + name +']');
+             if (input) input.value = data[name];
+
+            }
 
         }, configs);
 
@@ -68,6 +74,8 @@ class grid {
         }
 
     }
+
+    
 
     initButtons() {
 
